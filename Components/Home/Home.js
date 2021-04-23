5187261e785cdd8c1681deaac2ebf347ef85299c
rachel1306 <60882090+rachel1306@users.noreply.github.com>
2021-04-23 17:44:43 +0530

    Navbar Added

--- Components/Home/Home.js
+++ Components/Home/Home.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from '../Navbar/Navbar';
 import './Home.css';
 function Home() {
     return (
-        <div>
+        <div className='bg'>
             Home Page
+            <BrowserRouter>
+                <Navbar />
+            </BrowserRouter>
         </div>
     )
 }
--- Components/Navbar/Navbar.js
+++ Components/Navbar/Navbar.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React,{ useState } from 'react';
+import './Navbar.css'
+import { NavHashLink as Link } from 'react-router-hash-link';
+import { FaTwitter } from 'react-icons/fa';
+import { RiInstagramFill } from 'react-icons/ri';
+import { AiFillYoutube } from 'react-icons/ai';
+import { FaSpotify } from 'react-icons/fa';
+import { FaBars } from 'react-icons/fa';
+import { FaTimes } from 'react-icons/fa';
 function Navbar() {
+    //state={ clicked:false }  <i className={this.state.clicked?'fas fa-times':'fas fa-bars'}></i>
+    const [clicked,setClicked]=useState(false);
+    let menu,menuMask;
+    if(clicked){
+        menu=<div className='fixed bg-aqua h-full'> <FaTimes className='close'/> </div>
+        menuMask=<div onClick={() => setClicked(false)}></div>
+    }
+    else{
+        menu=<div> <FaBars /> </div>
+    }
     return (
-        <div>
-            Navbar
+        <div className='pos'>Navbar
         </div>
     )
 }
